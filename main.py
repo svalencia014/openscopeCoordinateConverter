cadf9d5d93045d9155ad82e8b63e3d3e85644fe4
Samuel Valencia <68522351+svalencia014@users.noreply.github.com>
2023-01-23 14:13:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ f.write("")
 f.close()
 
 for line in data:
+    if line.startswith('#'):
+        pass
     lineData = line.split(" ")
     #   Process line
     #   Remove Degree Symbol
@@ -18,9 +20,10 @@ for line in data:
     else:
         latitude = lineData[0].split(".", 2)
         longitude = lineData[1].split(".", 2)
-    if len(latitude[1]) != 2:
+    
+    if len(latitude[1]) != 3:
         latitude[1] = "0" + latitude[1]
-    if len(longitude[1]) != 2:
+    if len(longitude[1]) != 3:
         longitude[1] = "0" + longitude[1]
 
     lineOut = f"[\"{latitude[0]}d{latitude[1]}m{latitude[2]}\", \"{longitude[0]}d{longitude[1]}m{longitude[2]}\"]"
