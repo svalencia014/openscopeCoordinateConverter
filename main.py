3bb82be0662107cd735961b68519cf6bbd9a4081
Samuel Valencia <68522351+svalencia014@users.noreply.github.com>
2023-01-23 13:21:09 -0500

    update

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 path = input("What directory holds input.txt? ")
 with open(path + "/input.txt", "r") as f:
     data = f.read().splitlines()
+    f.close()
+f = open(path + "/output.txt", "w")
+f.write("")
+f.close()
 
 for line in data:
     lineData = line.split(" ")
-    latitude = lineData[0].split(".")
-    longitude = lineData[1].split(".")
-    if latitude[1].__len__ != 2:
+    #   Process line
+    #   Remove Degree Symbol
+    if lineData.__contains__("°"):
+        latitude = lineData[0].split("°")
+        longitude = lineData[1].split("°")
+        latitude = lineData[0].split("'")
+        longitude = lineData[1].split("'")
+    else:
+        latitude = lineData[0].split(".", 2)
+        longitude = lineData[1].split(".", 2)
+    if len(latitude[1]) != 2:
         latitude[1] = "0" + latitude[1]
-    if longitude[1].__len__ != 2:
+    if len(longitude[1]) != 2:
         longitude[1] = "0" + longitude[1]
-    lineOut = f"[{latitude[0]}d{latitude[1]}m{latitude[2]}], [{longitude[0]}d{longitude[1]}m{longitude[2]}]"
-    print(lineOut)
+
+    lineOut = f"[\"{latitude[0]}d{latitude[1]}m{latitude[2]}\", \"{longitude[0]}d{longitude[1]}m{longitude[2]}\"]"
+    output = open(path + "/output.txt", "a")
+    output.write(lineOut + "\n")
 
  
